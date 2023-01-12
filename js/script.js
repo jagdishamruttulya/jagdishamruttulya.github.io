f943cc50fc1337204306357bdb55a09665c215db
Piyush Mulatkar <piyushmulatkar11@gmail.com>
2023-01-13 01:41:37 +0530

    Added responsive menu and updated few components

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const toggleBackground = document.getElementById('navbar'); 
+const toggleMenu = document.getElementById('static-menu');
 
 function changeBG() {
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
@@ -6,6 +7,10 @@ function changeBG() {
   } else {
     toggleBackground.className = "";
   }
+}
+function changeToStaticMenu(x) {
+  x.classList.toggle("change");
+  toggleMenu.hidden=!toggleMenu.hidden;
 }
 window.addEventListener('scroll',() => {
 	// if( window.innerHeight + window.scrollY >= document.body.offsetHeight -200){
